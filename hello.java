bbc18d51e67a460078d263313fd0a43cb8fb0e7b
nanxiahuma <lifeng860115@163.com>
2016-04-07 13:47:43 +0800

    Create hello.java
    Create hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+class tank{
+	int level;
+}
+
+public class test{
+	public static void main(String[] args){
+		tank t1 = new tank();
+		tank t2 = new tank();
+		t1.level = 2;
+		t2.level = 5;
+		System.out.print("hello Java");
+		System.out.print("t1.leve=" + t1.level+"\n");
+		System.out.print("it is my first Java program");
+	}
+}
